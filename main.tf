5d10ee965f3479b41f65052fe8ff07f78c80dd88
micha <micha.luginbuehl@ipt.ch>
2025-09-30 17:09:20 +0200

    Rename module

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-module "terraform-aws-s3-webapp-lugm" {
+module "s3-webapp" {
   source  = "app.terraform.io/terraform-learn-lugm/terraform-aws-s3-webapp-lugm/myprovider"
   name    = var.name
   region  = var.region
